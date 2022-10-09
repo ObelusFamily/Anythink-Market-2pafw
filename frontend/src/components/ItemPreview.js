a76cce8ff760ce5906a4b5de51e68c204612d84d
Shimrit Snapir <bshimrit@gmail.com>
2022-10-09 17:35:01 +0300

    fix lint

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -4,7 +4,7 @@ import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
 
-const IMAGE_PLACEHOLDER = "placeholder.png"
+const IMAGE_PLACEHOLDER = "placeholder.png";
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
